fb1898f6f9ab99dfc1294e86969966e05d2e4979
bonorol <bonorol@naver.com>
2020-03-25 13:24:54 +0900

    add file

---
+++ rg.tf
@@ -0,0 +1,16 @@
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x. 
+  # If you are using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
+  features {}
+}
+
+
+resource "azurerm_resource_group" "myterraformgroup" {
+    name     = "${var.rgname}"
+    location = "koreacentral"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
